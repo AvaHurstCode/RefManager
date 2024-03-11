a8e02632a6b48ac8c02f9be24d2618d87204bc41
Ava Hurst <ava.hurst.home@gmail.com>
2024-03-11 13:11:55 +0100

    Set up static site through express server

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.listen(4000)
+app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-    console.log('Here')
-    res.json({message: "Error"})
-})
+app.listen(4000)
