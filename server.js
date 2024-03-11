1e4af333495ce2cc87259d40de564b0f119a5bd0
Ava Hurst <ava.hurst.home@gmail.com>
2024-03-11 15:17:00 +0100

    Added ejs file for dynamically generated html content

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.use(express.static("public"))
+
+app.get('/user/:userId/projects', (req, res) => {
+    res.render("projects", {listLength: req.params.userId})
+})
+
+app.get('*', (req, res) => {
+    res.sendStatus(404)
+})
 
 app.listen(4000)
