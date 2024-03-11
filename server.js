17d39493c3ef5b58a7abb3640a2cf750ef961592
Ava Hurst <ava.hurst.home@gmail.com>
2024-03-11 19:54:31 +0100

    Added second static route for the editor

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/user/:userId/projects', (req, res) => {
     res.render("projects", {listLength: req.params.userId})
 })
 
+app.get('/user/:userId/editor/project/:projectName', (req, res) => {
+    res.render("editor", {projectName : req.params.projectName})
+})
+
 app.get('/:slug/*', (req, res) => {
     res.status(404).send(`Error 404 ${req.params.slug} not found`)
 })
