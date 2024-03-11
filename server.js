075a0a4cb1f08bf5bc1e01f2a638acb6b53a0208
Ava Hurst <ava.hurst.home@gmail.com>
2024-03-11 20:35:28 +0100

    Added redirect from home to index and updated 404 routes

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ app.set("view engine", "ejs")
 
 app.use(express.static("public"))
 
+app.get('/home', (req, res) => {
+    res.redirect('')
+})
+
 app.get('/user/:userId/projects', (req, res) => {
     res.render("projects", {listLength: req.params.userId})
 })
 
 app.get('/user/:userId/editor/project/:projectName', (req, res) => {
     res.render("editor", {projectName : req.params.projectName, userId : req.params.userId})
+})
+
+app.get('/:slug', (req, res) => {
+    res.status(404).send(`Error 404 ${req.params.slug} not found`)
 })
 
 app.get('/:slug/*', (req, res) => {
