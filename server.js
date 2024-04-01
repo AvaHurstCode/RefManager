3a7efc5129f91cd82b13387694f6f4a3b9d90596
Ava Hurst <ava.hurst.home@gmail.com>
2024-04-01 22:21:18 +0200

    Converted all html files to ejs files for consistency

--- server.js
+++ server.js
@@ -1,34 +1,49 @@
 const express = require('express')
 const app = express()
+const { connectToDb, getDb } = require('./db')  
 
 const data = require('./data.json')
+
+//db connection
+let db
+
+connectToDb((err) => {
+    if (!err) {
+        app.listen(4000, () => {
+            console.log('app listening on port 4000')
+        })
+        db = getDb()
+        console.log(db)
+    }
+})
 
 app.set("view engine", "ejs")
 
 app.use(express.static("public"))
 
-app.get('/home', (req, res) => {
-    res.redirect('/index.html')
+app.get('/', (req, res) => {
+    res.render("index", {})
 })
 
 app.get('/user/:userId/projects', (req, res) => {
-    res.render("projects", {listLength: req.params.userId})
+    let projects = []
+
+    db.collection('projects')
+    .find()
+    .sort({author: 1})
+    .forEach(project => projects.push(project))
+    .then(() => {
+        res.status(200).render("projects", {projects: projects})
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Could not fetch the documents'})
+    })
 })
 
 app.get('/user/:userId/editor/project/:projectName', (req, res) => {
     res.render("editor", {projectName : req.params.projectName, userId : req.params.userId})
 })
 
-app.get('/jsonData', (req, res) => {
-    res.send(`The best food in the world is ${data.title} Description: ${data.description}`)
-})
-
-app.get('/:slug', (req, res) => {
+app.get(['/:slug', '/:slug/*'], (req, res) => {
     res.status(404).send(`Error 404 ${req.params.slug} not found`)
 })
-
-app.get('/:slug/*', (req, res) => {
-    res.status(404).send(`Error 404 ${req.params.slug} not found`)
-})
-
-app.listen(4000)
